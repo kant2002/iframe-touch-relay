70a22281e16062c6bb6d16d29fd73c0d92f76032
Andrii Kurdiumov <kant2002@gmail.com>
2024-02-06 19:13:09 +0600

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export function attachTouchRelay(): void;
+export function detachTouchRelay(): void;
+export function attachRelayToPage(my_zones: any): void;
+export function detachRelayToPage(): void;
